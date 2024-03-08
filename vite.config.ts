c611b8c76eb3b51d1df0d71f10a2348918854877
Destin <berniedestin7@gmail.com>
2024-03-08 15:30:45 -0500

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/terminal-page/",
+  base: "/terminal-page-vue/",
   plugins: [
     vue(),
   ],
